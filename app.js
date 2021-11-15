d226ebf5c682c995d3fe7a90197be6f76761c087
Nevyana Momchilova <nevyana.dm@gmail.com>
2021-11-15 16:28:26 +0200

    ways to get html elements

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
+const paragraph = document.querySelector("p");
+const ps = document.querySelectorAll("p");
 
+const title = document.getElementById("page-title");
+
+const errors = document.getElementsByClassName("error");
+
+const paras = document.getElementsByTagName("p");
+
+console.log(paras);
 
