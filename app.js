c16c3f4eb4ecce648a3b4e78d370d967967c5c08
Nevyana Momchilova <nevyana.dm@gmail.com>
2021-11-15 17:24:47 +0200

    innerText and innerHTML

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
+// const paragraph = document.querySelector("p");
+// const ps = document.querySelectorAll("p");
+
+// const title = document.getElementById("page-title");
+
+// const errors = document.getElementsByClassName("error");
+
+// const paras = document.getElementsByTagName("p");
+
+// console.log(paras);
+
 const paragraph = document.querySelector("p");
-const ps = document.querySelectorAll("p");
+paragraph.innerText = "hello user!";
+paragraph.innerText += " Have a nice day :)";
+console.log(paragraph);
 
-const title = document.getElementById("page-title");
 
-const errors = document.getElementsByClassName("error");
+const content = document.querySelector(".content");
+content.innerHTML = "<h2>this is new content h2</h2>";
 
-const paras = document.getElementsByTagName("p");
 
-console.log(paras);
+const people = ["mario", "luigi", "yoshi"];
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`;
+});
+
+
+
+
 
